c60b18429d4853ced23e43d22f9883b8028ad651
lequangvinh_2180603246 <153148687+vinh2233@users.noreply.github.com>
2025-04-23 09:22:00 +0700

    upcode fe
    build UI for user login and registry, build UI for CRUD task

--- server/src/middlewares/logger.ts
+++
@@ -1,6 +0,0 @@
-
-
-export const logger = (req: any, res: any, message: any) => {
-  console.log(`${req.method} ${req.url}`, message);
-  // next();
-};
--- server/src/utils/hash.ts
+++
@@ -1,2 +0,0 @@
-export const hashPassword = (password: string) => password;
-export const comparePassword = (password: string, hash: string) => password === hash;
